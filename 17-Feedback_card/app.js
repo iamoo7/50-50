6d576ae8a689c771497a816c5684e14a421f7a43
iamoo7 <ns991989@gmail.com>
2021-09-04 13:51:14 +0530

    Functions added

--- 17-Feedback_card/app.js
+++ 17-Feedback_card/app.js
@@ -1,15 +1,16 @@
 const card = document.querySelectorAll('.card')
+const panel = document.querySelector('.feedback-container')
 const close = document.querySelector('.close')
 const skip = document.querySelector('.btn_sec')
 const submit = document.querySelector('.btn_prim')
+const result = document.querySelector('#submitted')
 let rating = 'Satisfied'
 
 card.forEach((el) => {
     el.addEventListener('click', () =>{
         removeActiveClass()
         el.classList.add('active')
-        console.log(el)
-        rating = el.nextElementSibling.innerHTML
+        rating = el.innerText
         console.log(rating)
     })
 });
@@ -22,3 +23,7 @@ function removeActiveClass(){
     
 }
 
+submit.addEventListener('click', () =>{
+    result.style.display = 'flex'
+    panel.style.display = 'none'
+})
