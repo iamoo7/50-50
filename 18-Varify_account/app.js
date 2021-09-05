d5fb5a412c07a3fcc2c14d67f886e0a6c4d30adc
iamoo7 <ns991989@gmail.com>
2021-09-05 22:55:32 +0530

    input function updated

--- 18-Varify_account/app.js
+++ 18-Varify_account/app.js
@@ -1,12 +1,27 @@
 const timer = document.querySelector('#timer')
 const btnPrim = document.querySelector('.btn_primary')
+const input = document.querySelectorAll('.number_input')
 
-let i = 60;                  
+let i = 60;   
+
+input.forEach((code, idx) => {
+  code.addEventListener('keydown', (e) => {
+    if(e.key >= 0 && e.key <= 9){
+      input[idx].value = ''
+      setTimeout(() => input[idx + 1].focus(), 10 ) 
+  
+    } else if(e.key ==='Backspace'){
+    setTimeout(() => input[idx -1].focus(), 10)
+  }
+})
+  
+})        
 
 function myLoop() {         
   setTimeout(function() {   
-      timer.innerText = i 
-    i--;                   
+      timer.innerHTML = i 
+    i--; 
+                      
     if (i>=0) { 
         if(i==0){
             btnPrim.classList.add('resend_code')
@@ -25,7 +40,10 @@ btnPrim.addEventListener('click', () =>{
 })
 
 function resetBtn(){
+    i = 60
     btnPrim.classList.remove('resend_code')
     btnPrim.innerHTML = `Resend Code - <span id="timer">60</span> sec.`
-    i = 60
+    
 }
+
+
