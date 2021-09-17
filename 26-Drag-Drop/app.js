fc1f0bf12d4b5b4f219ec14fa7cfd21a21879579
iamoo7 <ns991989@gmail.com>
2021-09-17 18:26:44 +0530

    Initial commit

---
+++ 26-Drag-Drop/app.js
@@ -0,0 +1,43 @@
+const fill = document.querySelector('.fill')
+const empties = document.querySelectorAll('.empty')
+
+
+fill.addEventListener('dragstart', dragStart)
+fill.addEventListener('dragend', dragEnd)
+
+for(const empty of empties){
+    empty.addEventListener('dragover', dragOver)
+    empty.addEventListener('dragenter', dragEnter)
+    empty.addEventListener('dragleave', dragLeave)
+    empty.addEventListener('drop', dragDrop)
+}
+
+function dragStart(){
+    this.className += ' hold'
+    setTimeout(()=> this.className = ' invisible', 10);
+    
+}
+
+function dragEnd(){
+    this.className = 'fill'
+}
+
+function dragOver(e){
+    console.log('drag over')
+    e.preventDefault()
+}
+
+function dragEnter(e){
+    this.className += ' hovered'
+    e.preventDefault()
+}
+
+function dragLeave(){
+    this.className = 'empty'
+}
+
+function dragDrop(){
+    this.className = 'empty'
+    this.append(fill)
+}
+
